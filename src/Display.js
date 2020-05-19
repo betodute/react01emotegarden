90d98d1adeb626b480ee9c1085c3fab87d54e28b
betodute <betodute@gmail.com>
2020-05-18 18:27:03 -0700

    First Name and Last Name Form Structure with Display

--- src/Display.js
+++ src/Display.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Display extends React.Component {
   render() {
     return (
-      <h1> Test Display Component </h1>
+      <div>
+        <h1> {this.props.formData.firstName} </h1>
+        <h1> {this.props.formData.lastName} </h1>
+      </div>
     )
   }
 }
--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,30 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <h1> Test Form Component </h1>
+      <div>
+        <form>
+          <label>
+            First Name:
+            <input
+              type="text"
+              onChange={event => this.props.changeFirstName(event)}
+              value={this.props.formData.firstName}
+            />
+          </label>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              onChange={event => this.props.changeLastName(event)}
+              value={this.props.formData.lastName}
+            />
+          </label>
+          <label>
+            Plants:
+            <select></select>
+          </label>
+        </form>
+      </div>
     )
   }
 }
